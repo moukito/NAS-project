f18d9f4b0d2ed92d0cd10df1506d2f188803d00f
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-07 15:53:01 +0100

    début de parser et fichier ipv6 mais enfait on va ptet pas en avoir besoin parce que le module standard ipaddress a déjà tout

--- code/parser.py
+++ code/parser.py
@@ -1,4 +1,5 @@
 import json
+import ipaddress
 
 from autonomous_system import AS
 from router import Router
@@ -10,5 +11,11 @@ def parse_intent_file(file_path:str) -> tuple[list[AS], list[Router]]:
     with open(file_path, "r") as file:
         data = json.load(file)
         les_as = []
-        for as in data["Les"]
+        for autonomous in data[AS_LIST_NAME]:
+            ip = ipaddress.IPv6Network(autonomous["ipv6_prefix"])
+            as_number = autonomous["AS_number"]
+            routers = autonomous["routers"]
+            routage_interne = autonomous["routage_interne"],
+            connected_as = autonomous["AS_connectes"]
+            les_as.append(AS())
     return ("", "")
