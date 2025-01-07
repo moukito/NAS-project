5cc31f7076cba3a5f2dcd75d95d01d9894c9551a
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-07 15:32:32 +0100

    début de code

--- code/autonomous_system.py
+++ code/autonomous_system.py
@@ -1,3 +1,12 @@
 class AS:
-    def __init__(self):
-        self.cool = "cool"
+    def __init__(self, as_number):
+        self.as_number = as_number
+
+    def print_as_number(self):
+        """
+        Cette fonction sert à print l'AS number
+
+        entrées: rien
+        sorties: output dans la console
+        """
+        print(self.as_number)
