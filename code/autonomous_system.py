181f223ee080ef79f9dfb0b3fc2750174f61ece8
MonsOnc61 <zhongyuye2001@gmail.com>
2025-01-07 15:45:13 +0100

    AS

--- code/autonomous_system.py
+++ code/autonomous_system.py
@@ -1,3 +1,7 @@
 class AS:
-    def __init__(self):
-        self.cool = "cool"
+    def __init__(self, ipv6_prefix, AS_number, routers, routage_interne, AS_connenctes):
+        self.ipv6_prefix = ipv6_prefix
+        self.AS_number = AS_number
+        self.routers = routers
+        self.routage_interne = routage_interne
+        self.AS_connenctes = AS_connenctes
