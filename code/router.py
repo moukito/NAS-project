3b0ac99afe1c90fbfe07403a76f2777a56b7592a
MonsOnc61 <zhongyuye2001@gmail.com>
2025-01-07 16:30:39 +0100

    router

--- code/router.py
+++ code/router.py
@@ -1,3 +1,6 @@
 class Router:
-    def __init__(self):
+    def __init__(self, hostname, liens, AS_number):
+        self.hostname = hostname
+        self.liens = liens
+        self.AS_number = AS_number
         
