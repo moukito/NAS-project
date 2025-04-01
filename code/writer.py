007f06eea88997828206ded45774fc80378539f6
moukito <theofremont50@gmail.com>
2025-04-01 13:11:11 +0200

    Add support for IPv4 handling in writer configuration generation

--- code/writer.py
+++ code/writer.py
@@ -7,6 +7,11 @@ IPV6_UNICAST_STRING = """no ip domain lookup
 ipv6 unicast-routing
 ipv6 cef
 """
+
+IPV4_UNICAST_STRING = """no ip domain lookup
+ip routing
+ip cef
+"""
 LOCAL_PREF_ROUTE_MAPS = """
 route-map tag_pref_provider permit 10
  set local-preference 100
@@ -24,10 +29,16 @@ def get_ospf_config_string(AS, router):
     entrées : AS: Autonomous System et router un Router
     sortie : str contenant la configuration correspondante
     """
-    ospf_config_string = f"ipv6 router ospf {NOM_PROCESSUS_IGP_PAR_DEFAUT}\n"
-    ospf_config_string += f" router-id {router.router_id}.{router.router_id}.{router.router_id}.{router.router_id}\n"# network {router.loopback_address}/128 area 0\n"
+    if router.ip_version == 6: # todo : a revoir
+        ospf_config_string = f"ipv6 router ospf {NOM_PROCESSUS_IGP_PAR_DEFAUT}\n"
+    else:
+        ospf_config_string = f"router ospf {NOM_PROCESSUS_IGP_PAR_DEFAUT}\n"
+    
+    ospf_config_string += f" router-id {router.router_id}.{router.router_id}.{router.router_id}.{router.router_id}\n"
+    
     for passive in router.passive_interfaces:
         ospf_config_string += f" passive-interface {passive}\n"
+    
     return ospf_config_string
 
 
@@ -38,10 +49,15 @@ def get_rip_config_string(AS, router):
     entrées : AS: Autonomous System et router un Router
     sortie : str contenant la configuration correspondante
     """
-    rip_config_string = f"ipv6 router rip {NOM_PROCESSUS_IGP_PAR_DEFAUT}\n"
+    if router.ip_version == 6: # todo : a revoir
+        rip_config_string = f"ipv6 router rip {NOM_PROCESSUS_IGP_PAR_DEFAUT}\n"
+    else:
+        rip_config_string = f"router rip\n version 2\n"
+    
     for passive in router.passive_interfaces:
-        pass
-        #rip_config_string += f" passive-interface {passive}\n"
+        if router.ip_version == 4:
+            rip_config_string += f" passive-interface {passive}\n"
+    
     return rip_config_string
 
 
@@ -53,20 +69,33 @@ def get_final_config_string(AS: AS, router: "Router", mode: str):
 	sortie : str contenant la configuration correspondante (bien complète, pas besoin de parsing ou de manipulation de string en +) OU liste de str de commandes si mode == "telnet"
 	"""
 	if mode == "telnet":
-		# todo : telnet command
 		return get_all_telnet_commands(AS, router)
+	
 	if AS.internal_routing == "OSPF":
 		internal_routing = get_ospf_config_string(AS, router)
 	else:
 		internal_routing = get_rip_config_string(AS, router)
+	
 	total_interface_string = ""
 	for config_string in router.config_str_per_link.values():
 		total_interface_string += config_string
+	
 	route_maps = ""
 	community_lists = AS.full_community_lists
 	for autonomous in router.used_route_maps:
 		route_maps += AS.community_data[autonomous]["route_map_in"]
 	route_maps += AS.global_route_map_out
+	
+	# Sélectionner la configuration unicast appropriée selon la version IP
+	if router.ip_version == 6: # todo : a revoir
+		unicast_config = "no ip domain lookup\nipv6 unicast-routing\nipv6 cef"
+		loopback_config = f"interface {STANDARD_LOOPBACK_INTERFACE}\n no ip address\n negotiation auto\n ipv6 enable\n ipv6 address {router.loopback_address}/128\n {router.internal_routing_loopback_config}"
+		forward_protocol = "ip forward-protocol nd"
+	else:
+		unicast_config = "no ip domain lookup\nip routing\nip cef"
+		loopback_config = f"interface {STANDARD_LOOPBACK_INTERFACE}\n no ipv6 address\n negotiation auto\n ip address {router.loopback_address} 255.255.255.255\n {router.internal_routing_loopback_config}"
+		forward_protocol = "ip forward-protocol nd"
+	
 	return f"""!
 !
 !
@@ -93,9 +122,7 @@ ip cef
 !
 !
 !
-no ip domain lookup
-ipv6 unicast-routing
-ipv6 cef
+{unicast_config}
 !
 !
 multilink bundle-name authenticated
@@ -123,18 +150,13 @@ ip bgp-community new-format
 !
 !
 !
-interface {STANDARD_LOOPBACK_INTERFACE}
- no ip address
- negotiation auto
- ipv6 enable
- ipv6 address {router.loopback_address}/128
- {router.internal_routing_loopback_config}
+{loopback_config}
 !
 !
 {total_interface_string}
 {router.config_bgp}
 !
-ip forward-protocol nd
+{forward_protocol}
 !
 !
 no ip http server
@@ -182,12 +204,15 @@ def get_all_telnet_commands(AS:AS, router:"Router"):
 		route_maps_setup = liste_raw[:len(liste_raw) - 3] + ["exit"] + [liste_raw[-3]] + ["exit"]
 	else:
 		route_maps_setup = [AS.global_route_map_out.split("\n")[0]] + ["exit"]
+	
 	if AS.internal_routing == "OSPF":
 		internal_routing = get_ospf_config_string(AS, router).split("\n") + ["exit"]
 	else:
 		internal_routing = get_rip_config_string(AS, router).split("\n") + ["exit"]
+	
 	bgp_setup = router.config_bgp.split("\n")
 	loopback_setup = router.internal_routing_loopback_config.split("\n") + ["exit"]
+	
 	for autonomous in router.used_route_maps:
 		route_maps_setup += AS.community_data[autonomous]["route_map_in"].split("\n")
 		route_maps_setup += ["exit"]
@@ -195,8 +220,14 @@ def get_all_telnet_commands(AS:AS, router:"Router"):
 	interface_configs = []
 	for interface in router.config_str_per_link.values():
 		interface_configs += interface.split("\n")
-	final = (["config t", "ip bgp-community new-format",
-	          "ipv6 unicast-routing"] + community_list_setup + route_maps_setup + internal_routing + loopback_setup + interface_configs + bgp_setup)
+	
+	# Configuration initiale selon la version IP
+	if router.ip_version == 6: # todo : a revoir
+		initial_config = ["config t", "ip bgp-community new-format", "ipv6 unicast-routing"]
+	else:
+		initial_config = ["config t", "ip bgp-community new-format", "ip routing"]
+	
+	final = (initial_config + community_list_setup + route_maps_setup + internal_routing + loopback_setup + interface_configs + bgp_setup)
 	for commande in list(final):
 		if "!" in commande:
 			final.remove(commande)
